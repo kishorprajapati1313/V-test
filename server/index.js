13cf34d2e639f50319e79cb7f66b5bb505734928
Kishor1212 <kishorprajapati1014@gmail.com>
2024-08-20 18:29:35 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -34,9 +34,7 @@ app.use(cors({
 app.use(express.json({ limit: '1000mb' }));
 app.use(express.urlencoded({ limit: '1000mb', extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/commondb", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect("mongodb+srv://xc:xc1010%40gmail@cluster0.32ogv.mongodb.net/commondb", {
 }).then(() => {
   console.log("MongoDB connected successfully");
 }).catch(err => {
