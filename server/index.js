f59f3bf570a5bc444ab8a00ad4adf8aee293040c
Kishor1212 <144512766+kishorprajapati1212@users.noreply.github.com>
2024-09-19 19:17:40 +0530

    _pp_last_7_up

--- server/index.js
+++ server/index.js
@@ -17,9 +17,15 @@ const allowedOrigins = [
 
 // Middleware to handle CORS
 app.use(cors({
-  origin: "https://regal-sunshine-279c0c.netlify.app",
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allow OPTIONS for preflight
-  credentials: true  // Allow credentials such as cookies or authorization headers
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    const msg = 'The CORS policy does not allow access from this origin.';
+    return callback(new Error(msg), false);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,  // Allow credentials
 }));
 
 // Preflight handling for all routes
