e3e4c2352d959ab57f47a7fff86f992a52d176c6
Kishor1212 <144512766+kishorprajapati1212@users.noreply.github.com>
2024-09-19 19:04:20 +0530

    forgot_last_6_up

--- server/index.js
+++ server/index.js
@@ -17,15 +17,7 @@ const allowedOrigins = [
 
 // Middleware to handle CORS
 app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: "https://regal-sunshine-279c0c.netlify.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Allow OPTIONS for preflight
   credentials: true  // Allow credentials such as cookies or authorization headers
 }));
